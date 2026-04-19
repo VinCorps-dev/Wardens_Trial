956e6aedcfddfc8a08c474804b2674883950f698
VinCorps-dev <vincentbillybob@gmail.com>
2026-04-19 14:33:27 +0800

    Gumagana na ang spike collision

---
+++ Assets/Tilesets/Tiles.tsx
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Dungeon Tile Set" tilewidth="16" tileheight="16" tilecount="270" columns="15">
+ <image source="../../Tilesets/Dungeon Tile Set.png" width="240" height="288"/>
+ <tile id="16" type="Tiles"/>
+ <tile id="17" type="Tiles"/>
+ <tile id="18" type="Tiles"/>
+ <tile id="20" type="Tiles"/>
+ <tile id="21" type="Tiles"/>
+ <tile id="31" type="Tiles"/>
+ <tile id="32" type="Tiles"/>
+ <tile id="33" type="Tiles"/>
+ <tile id="35" type="Tiles"/>
+ <tile id="36" type="Tiles"/>
+ <tile id="46" type="Tiles"/>
+ <tile id="47" type="Tiles"/>
+ <tile id="48" type="Tiles"/>
+ <tile id="50" type="Tiles"/>
+ <tile id="51" type="Tiles"/>
+ <tile id="76" type="Tiles"/>
+ <tile id="77" type="Tiles"/>
+ <tile id="78" type="Tiles"/>
+ <tile id="91" type="Tiles"/>
+ <tile id="92" type="Tiles"/>
+ <tile id="93" type="Tiles"/>
+ <tile id="106" type="Tiles"/>
+ <tile id="107" type="Tiles"/>
+ <tile id="108" type="Tiles"/>
+ <tile id="211" type="Deadly">
+  <properties>
+   <property name="Name: type&#10;Value: deadly" value=""/>
+  </properties>
+ </tile>
+ <tile id="212" type="Deadly">
+  <properties>
+   <property name="Name: type&#10;Value: deadly">Name: type
+Value: deadly</property>
+  </properties>
+ </tile>
+ <tile id="213" type="Deadly">
+  <properties>
+   <property name="Name: type&#10;Value: deadly" value=""/>
+  </properties>
+ </tile>
+ <tile id="214" type="Deadly">
+  <properties>
+   <property name="Name: type&#10;Value: deadly">Name: type
+Value: deadly</property>
+  </properties>
+ </tile>
+ <tile id="215" type="Deadly">
+  <properties>
+   <property name="Name: type&#10;Value: deadly" value=""/>
+  </properties>
+ </tile>
+ <tile id="216" type="Deadly">
+  <properties>
+   <property name="Name: type&#10;Value: deadly">Name: type
+Value: deadly</property>
+  </properties>
+ </tile>
+</tileset>
