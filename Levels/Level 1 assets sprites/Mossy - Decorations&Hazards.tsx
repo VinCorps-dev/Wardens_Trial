c02a45c4f60ddc34354bf8ddc8039fe8e84d7627
VinCorps-dev <vincentbillybob@gmail.com>
2026-04-29 13:59:25 +0800

    May inbetween na and may pic at text na sya and working na yung level manager

---
+++ Levels/Level 1 assets sprites/Mossy - Decorations&Hazards.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Mossy - Decorations&amp;Hazards" tilewidth="16" tileheight="16" tilecount="65536" columns="256">
+ <image source="../../Assets/Background For Levels/Level 1/Mossy - Decorations&amp;Hazards.png" width="4096" height="4096"/>
+</tileset>
