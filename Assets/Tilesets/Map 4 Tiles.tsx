5f49c97c34cfcf747b7bd446de832690f85a8746
VinCorps-dev <vincentbillybob@gmail.com>
2026-06-12 08:51:46 +0800

    Completed Levels And my checkpoint detection na and working na lahat ng characters at gem

---
+++ Assets/Tilesets/Map 4 Tiles.tsx
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Tiny Tower FREE" tilewidth="16" tileheight="16" tilecount="540" columns="36">
+ <image source="Map 4/Tiny Tower Free Pack/Tiny Tower FREE.png" width="576" height="240"/>
+ <tile id="74" type="Tiles"/>
+ <tile id="75" type="Tiles"/>
+ <tile id="76" type="Tiles"/>
+ <tile id="77" type="Tiles"/>
+ <tile id="81">
+  <properties>
+   <property name="type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <properties>
+   <property name="type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="146" type="Tiles"/>
+ <tile id="147" type="Tiles"/>
+ <tile id="148" type="Tiles"/>
+ <tile id="149" type="Tiles"/>
+ <tile id="153">
+  <properties>
+   <property name="type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="155">
+  <properties>
+   <property name="type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="157">
+  <properties>
+   <property name="type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="218" type="Tiles"/>
+ <tile id="219" type="Tiles"/>
+ <tile id="220" type="Tiles"/>
+ <tile id="221" type="Tiles"/>
+ <tile id="225">
+  <properties>
+   <property name="type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="227">
+  <properties>
+   <property name="type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="229">
+  <properties>
+   <property name="type" value="platform"/>
+  </properties>
+ </tile>
+ <tile id="441">
+  <properties>
+   <property name="type" value="tiles"/>
+  </properties>
+ </tile>
+</tileset>
